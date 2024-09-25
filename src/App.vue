34ad199a7bf730a32df2babae0c75561fbd1783e
Meryam El Boudouti <meryam@opencertif.fr>
2024-09-25 14:50:03 +0200

    added welcome comp

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app" class="min-h-screen flex flex-col">
+  <div id="app" class="min-h-screen w-full flex flex-col">
     <NavigationBar />
+    <Welcome />
     <OfferingsComponent class="flex-grow" />
     <FooterComponent />
   </div>
@@ -11,6 +12,7 @@ import { defineComponent } from 'vue';
 import NavigationBar from './components/NavigationBar.vue';
 import FooterComponent from './components/FooterComponent.vue';
 import OfferingsComponent from './components/OfferingsComponent.vue';
+import Welcome from './components/Welcome.vue';
 
 export default defineComponent({
   name: 'App',
@@ -18,6 +20,7 @@ export default defineComponent({
     NavigationBar,
     FooterComponent,
     OfferingsComponent,
+    Welcome,
   },
 });
 </script>
@@ -25,22 +28,15 @@ export default defineComponent({
 <style scoped>
 html, body {
   height: 100%;
-  width: 100vw; /* Ensure the body takes the full viewport width */
+  width: 100%; /* Ensure full width */
   margin: 0;
   padding: 0;
-  overflow-x: hidden; /* Prevent horizontal scroll if content overflows */
 }
 
 #app {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-  width: 100vw; /* Use viewport width to ensure full stretch across the screen */
-}
-
-main {
-  flex-grow: 1;
-  width: 200vw; /* Ensure main content also stretches fully across */
-  margin: 0; /* Remove any default margin */
+  width: 100%; /* Make sure the app takes full width */
 }
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+      <div class="section-container">
+        <!-- Background image -->
+        <img
+          loading="lazy"
+          src="../assets/images/paris.webp"
+          alt=""
+          class="background-img"
+        />
+        <!-- Content -->
+        <div class="header-content">
+          <header>
+            <h1>Cabinet Gildas Laffaille</h1>
+            <p>AXA Prévoyance & Patrimoine</p>
+            <hr class="border-0 h-[2.5px] w-full max-w-[500px] bg-[#9e827b]">
+          </header>
+          <h2>Prévoyance</h2>
+          <h2>Mutuelle Retraite</h2>
+          <div class="flex flex-col items-start">
+            <hr class="border-0 h-[2.5px] w-full max-w-[500px] bg-[#9e827b]">
+            <p>
+              Protection sociale et gestion de patrimoine pour les Particuliers,
+              Professions libérales ou Entreprises.
+            </p>
+            <button>DEMANDER UN DEVIS</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import '../assets/welcome.css';
+
+  
+  export default defineComponent({
+    name: 'Welcome',
+  });
+  </script>
+  

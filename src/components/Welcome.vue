ae2fa3949bea4d401b563cb932db1bb85a0089f7
Meryam El Boudouti <meryam@opencertif.fr>
2024-10-02 16:29:55 +0200

    style welcome component to fit mockup

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,28 +1,32 @@
-<template>
-    <section>
-      <div class="section-container">
+
+  <template>
+    <section class="hero-section">
+      <div class="hero-content">
         <img
           loading="lazy"
           src="../assets/images/paris.webp"
           alt=""
-          class="background-img"
+          class="hero-background"
         />
         <!-- Content -->
-        <div class="header-content">
-          <header>
-            <h1>Cabinet Gildas Laffaille</h1>
-            <p>AXA Prévoyance & Patrimoine</p>
-            <hr class="border-0 h-[2.5px] w-full max-w-[500px] bg-[#9e827b]">
+        <div class="hero-text-container">
+          <header class="hero-header">
+            <h1 class="company-name">Cabinet Gildas Laffaille</h1>
+            <p class="company-subtitle">AXA Prévoyance & Patrimoine</p>
           </header>
-          <h2>Prévoyance</h2>
-          <h2>Mutuelle Retraite</h2>
-          <div class="flex flex-col items-start">
-            <hr class="border-0 h-[2.5px] w-full max-w-[500px] bg-[#9e827b]">
-            <p>
+          <hr class="border-0 h-[2.5px] w-full max-w-[500px] bg-[#9e827b] mb-5">
+          <div class="hero-titles">
+          <h2 >Prévoyance</h2>
+          <h2 >Mutuelle</h2>
+          <h2 >Retraite</h2>
+          </div>
+          <hr class="border-0 h-[2.5px] w-full max-w-[500px] bg-[#9e827b] mt-5">
+          <div class="hero-cta-container">
+            <p class="hero-description">
               Protection sociale et gestion de patrimoine pour les Particuliers,
               Professions libérales ou Entreprises.
             </p>
-            <button>DEMANDER UN DEVIS</button>
+            <button class="cta-button">DEMANDER UN DEVIS</button>
           </div>
         </div>
       </div>
@@ -32,7 +36,6 @@
   <script lang="ts">
   import { defineComponent } from 'vue';
   import '../assets/welcome.css';
-
   
   export default defineComponent({
     name: 'Welcome',

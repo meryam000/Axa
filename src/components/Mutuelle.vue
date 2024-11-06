60bbe75d9a379b83e231bbbadb5d0752c1ea26c3
Meryam El Boudouti <meryam@opencertif.fr>
2024-11-06 16:50:19 +0100

    few changes

--- src/components/Mutuelle.vue
+++ src/components/Mutuelle.vue
@@ -195,7 +195,7 @@
                 </div>
             </div>
             <div class="et_pb_module et_pb_text et_pb_text_13  et_pb_text_align_left et_pb_bg_layout_light">
-                <div class="et_pb_text_inner">Jusqu’à 65 € par séance et 150 €/ an chez le psychothérapeute, ostéopathe, chiropracteur, acupuncteur et pédicure-podologue.</div>
+                <div class="et_pb_text_inner">Jusqu’à 65 € par séance et 150 €/ an chez le psychothérapeute, ostéopathe, chiropracteur, acupuncteur, pédicure et podologue.</div>
             </div>
         </div>
         <div class="et_pb_column et_pb_column_1_3 et_pb_column_12  et_pb_css_mix_blend_mode_passthrough">
